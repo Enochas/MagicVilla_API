9542fd99f2bcc6b97a4d7bc17b574a256eb412ac
enochas <enochasani5@gmail.com>
2025-11-01 18:05:06 +0100

    "Initial commit"

---
+++ MagicVilla_WebAPI/Logging/ILogging.cs
@@ -0,0 +1,9 @@
+﻿namespace MagicVilla_WebAPI.Logging
+{
+    public interface ILogging
+    {
+        public void Log(string message, string type);
+
+    }
+}
+    
---
+++ MagicVilla_WebAPI/Logging/Logging.cs
@@ -0,0 +1,16 @@
+﻿namespace MagicVilla_WebAPI.Logging
+{
+    public class Logging : ILogging
+    {
+        public void Log(string message, string type)
+        {
+            if (type == "error")
+            {
+                Console.WriteLine("Error_" + message);
+            }
+            {
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
